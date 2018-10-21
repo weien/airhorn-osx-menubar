ba58bad1e5cc96a92edc0ce4067f0259a102a22c
Sammy Gutierrez <me@sammygutierrez.com>
2018-10-21 12:30:47 -0500

    Initial Commit

---
+++ airhorn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  airhorn
+//
+//  Created by Sammy Gutiérrez on 10/21/18.
+//  Copyright © 2018 Sammy Gutiérrez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
